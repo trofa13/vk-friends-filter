07f123b81056bf4352f5d2ebe99df712dd0ce76d
trofa13 <trofa13@inbox.ru>
2016-03-30 06:34:46 +0300

    first connection to vk

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ VK.Auth.login(function (response){
 	}
 })
 
+
+VK.api('friends.get', {'name_case': 'nom', 'fields':'photo_100'}, function(response) {
+                if (response.error) {
+                  console.log('response errror')
+                } else {
+                //    allFriends.textContent = 'Музыка на странице ' + response.response[0].first_name + ' ' + response.response[0].last_name;
+                }
+                console.log(response)
+                
+                for (var i=0; i<response.response.length; i++){
+                	var li = document.createElement('li');
+                	li.textContent = response.response[i].first_name + ' ' + response.response[i].last_name;
+                	friendsList.appendChild(li);
+                }
+ });
+
+
